fbef25ab8ff4e7eae86277df3317db06c6424c43
David Batty <thestylelab@gmail.com>
2018-12-10 16:54:58 +0000

    Added validation to scripts to validate complete folders of data

--- Validator/Validator.py
+++ Validator/Validator.py
@@ -1,13 +1,30 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.getLogger('cutplace').setLevel(logging.WARNING)
-
 import os.path
 import cutplace
+import logging
+import cutplace.errors
+from pathlib import Path
 
-# Compute the path of a test file in a system independent manner,
-# assuming that the current folder is "docs".
-#cid_path = os.path.join(os.pardir, '', 'cid_customers.ods')
-cid = cutplace.Cid('cid_customers.ods')
 
-cutplace.validate('cid_customers.ods', 'customer.csv')
+#set up logging
+logging.basicConfig(level=logging.WARNING,filename='Validator/validation.log',filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
+logging.getLogger('cutplace').setLevel(logging.WARNING)
+
+#iterate through data folder
+p = Path("Validator/data")
+for i in p.glob('*.csv'):
+    # log file checked.
+    logging.info('%s' % ' checking file ' + i.name)
+    try:
+        for row_or_error in cutplace.rows('Validator/cid_customers.ods', 'Validator/data/' + i.name, on_error='yield'):
+            if isinstance(row_or_error, Exception):
+                if isinstance(row_or_error, cutplace.errors.CutplaceError):
+                    # log data related error details and move on.
+                    logging.warning('%s' % row_or_error )
+                else:
+                    # Let other, more severe errors terminate the validation.
+                    logging.error('%s' %  row_or_error )
+            else:
+                logging.info('%s'  %  row_or_error + ' Success')
+    except Exception as e:
+        print(e)
+        logging.critical("Exception occurred" + e, exc_info=True)
